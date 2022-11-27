4054ee5549d4022b0cca6c6316147715aca2ac90
Bartek <awprojektsender@gmail.com>
2022-11-27 19:59:15 +0100

    first one

---
+++ src/components/Naglowek.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Naglowek = () => {
+  return (
+    <div>
+      <h1>Nagłówek</h1>
+    </div>
+  )
+}
+
+export default Naglowek
+
---
+++ src/components/Zawartosc.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Zawartosc = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Zawartosc
