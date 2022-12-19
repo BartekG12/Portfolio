1aa2e8a5825abb194211d3004be8dfb93ee72dbd
BartekG12 <awprojektsender@gmail.com>
2022-12-19 09:11:32 +0100

    cv pdf

--- src/components/PasekKontaktu.js
+++ src/components/PasekKontaktu.js
@@ -4,16 +4,17 @@ import Linkedin from '../img/ikony/kontakt/linkedin.png';
 import Discord from '../img/ikony/kontakt/discord.png';
 import Cv from '../img/ikony/kontakt/cv.png';
 import Github from '../img/ikony/kontakt/github.png';
+import PDF from '../img/Bartosz Gurgul CV.pdf';
 
 const PasekKontaktu = ({id}) => {
   return (
     <div className='PasekKontaktu' id={id}>
-        <a target="_blank" href='mailto:bartoszgurguldev@gmail.com'><img className='Ikona' src={Mail} alt="Mail" /></a>
-        <a target="_blank" href='https://www.linkedin.com/in/bartosz-gurgul-b8a968233/'><img className='Ikona' src={Linkedin} alt="Linkedin" /></a>
-        <a target="_blank" href='https://discordapp.com/users/681999707522072705'><img className='Ikona' src={Discord} alt="Discord" /></a>
+        <a target="_blank" rel="noreferrer" href='mailto:bartoszgurguldev@gmail.com'><img className='Ikona' src={Mail} alt="Mail" /></a>
+        <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/bartosz-gurgul-b8a968233/'><img className='Ikona' src={Linkedin} alt="Linkedin" /></a>
+        <a target="_blank" rel="noreferrer" href='https://discordapp.com/users/681999707522072705'><img className='Ikona' src={Discord} alt="Discord" /></a>
         <span className='mobilnySeparatorKontaktu'></span>
-        <a target="_blank" href=''><img className='Ikona' src={Cv} alt="Cv" /></a>
-        <a target="_blank" href='https://github.com/BartekG12'><img className='Ikona' src={Github} alt="Github" /></a>
+        <a target="_blank" rel="noreferrer" href={PDF} download><img className='Ikona' src={Cv} alt="Cv" /></a>
+        <a target="_blank" rel="noreferrer" href='https://github.com/BartekG12'><img className='Ikona' src={Github} alt="Github" /></a>
         <div className='LiniaPionowa'></div>
     </div>
   )
